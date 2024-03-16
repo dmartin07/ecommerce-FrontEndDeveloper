c5bc968ceacad63e40c22e0ad4ba42edb451b1e0
dmartin07 <dariomartinbenavidez@hotmail.com>
2024-03-16 18:13:42 -0300

    Navbar

---
+++ src/components/ui/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import Logo from "../../../assets/logo.svg"
+import Cart from "../../../assets/cart.svg"
+import styles from "./Navbar.module.css"
+
+export const Navbar = () => {
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.navbarDetail}>
+        <img src={Logo} alt="Ecommerce Logo" width={50} height={50} />
+        <div>
+            <span>DH Ecommerce</span>
+        </div>
+      </div>
+
+      <div className={styles.navbarCartContainer}>
+        <p className={styles.navbarTextAmount}>2</p>
+        <img src={Cart} alt="Cart" />
+      </div>
+    </div>
+  );
+};
