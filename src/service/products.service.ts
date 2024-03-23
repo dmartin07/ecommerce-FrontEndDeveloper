d30ff3dc86897b7dda2f7dac24067f5876cdcf72
dmartin07 <dariomartinbenavidez@hotmail.com>
2024-03-23 11:32:11 -0300

    service

---
+++ src/service/products.service.ts
@@ -0,0 +1,16 @@
+import {Products} from '../interface'
+
+export const getProducts = async ():Promise<Products[]> => {
+  try {
+    const response = await fetch("http://localhost:3000/products");
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+        throw new Error('Fail to fetch products')
+    }
+  } catch (error) {
+    throw new Error('Network error');
+  }
+};
